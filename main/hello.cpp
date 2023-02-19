b01014b5c067b1d76ee0333f07b12f691548c722
blacjac21 <79352087+blacjac21@users.noreply.github.com>
2023-02-19 19:14:57 +0530

    ip

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n"
+  cout << "Hello, World\n";
   cout << "Hello, Jenkins this is great PES1UG20CS441\n";
   cout << "I have successfully built and run\n";
   return 0;
